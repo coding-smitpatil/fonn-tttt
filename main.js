e2ec9ead36f1c8706ebb7c8f3cd5e007ff1ddecb
steel peacock deeznutz <82657381+coding-smitpatil@users.noreply.github.com>
2022-05-02 06:07:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+nosex=0;
+nosey=0;
+rightwristx=0;
+leftwristx=0;
+difference=0;
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    canvas=createCanvas(500,500);
+    canvas.position(570,100);
+}
